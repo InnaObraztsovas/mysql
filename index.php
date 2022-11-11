abb1f3ec5f0571337faaf89d5c2fd35d84bf88f5
inna <inna.obraztsovas@gmail.com>
2022-11-11 14:43:53 +0200

    query

--- index.php
+++ index.php
@@ -12,19 +12,19 @@ $seeder->table('users')->columns([
     'id',
     'name' => $faker->name,
     'email' => $faker->email,
-    'birthday' => $faker->date('Y-m-d')
+    'birthday' => $faker->date('Y-m-d', '-18 years')
 ])->rowQuantity(100);
 
 
 $seeder->table('companies')->columns([
     'id',
     'name' => $faker->company
-])->rowQuantity(35);
+])->rowQuantity(30);
 
 $seeder->table('employees')->columns([
-    'user_id' => $generator->relation('users', 'id'),
+    'user_id' => array_unique($generator->relation('users', 'id')),
     'company_id' => $generator->relation('companies', 'id'),
-    'date_of_employment' => $faker->date('Y-m-d')
-])->rowQuantity(100);
+    'date_of_employment' => $faker->date('Y-m-d', 'now')
+])->rowQuantity(80);
 
 $seeder->refill();
--- index.sql
+++ index.sql
@@ -1,20 +1,22 @@
 use docker;
-create table users (
-    id int(10) AUTO_INCREMENT PRIMARY KEY,
-    name varchar(256),
-    email varchar(256) UNIQUE,
+create table users
+(
+    id       int(10) AUTO_INCREMENT PRIMARY KEY,
+    name     varchar(256),
+    email    varchar(256) UNIQUE,
     birthday date
 );
 
-create table companies(
-    id int(10) AUTO_INCREMENT PRIMARY KEY,
+create table companies
+(
+    id   int(10) AUTO_INCREMENT PRIMARY KEY,
     name varchar(256) UNIQUE
 );
 
 create table employees
 (
-    user_id int not null,
-    company_id int not null,
+    user_id            int  not null unique,
+    company_id         int  not null,
     date_of_employment date not null
 );
 
@@ -27,5 +29,51 @@ ALTER TABLE employees
     add constraint employees_users_id_fk
         foreign key (user_id) references users (id)
             ON DELETE CASCADE;
+
+
+select c.name, max(u.birthday), min(u.birthday)
+from companies as c
+         join employees e on c.id = e.company_id
+         join users u on u.id = e.user_id
+group by c.id;
+
+select c.name, round(avg(DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), u.birthday)), '%Y') + 0))
+from companies as c
+         join employees e on c.id = e.company_id
+         join users u on u.id = e.user_id
+group by c.id;
+
+
+select c.name, count(c.name)
+from companies as c
+         join docker.employees e on c.id = e.company_id
+         join docker.users u on u.id = e.user_id
+group by c.name
+order by count(*) desc
+limit 1;
+
+select c.name, max(e.date_of_employment)
+from users as u
+         join docker.employees e on u.id = e.user_id
+         join docker.companies c on c.id = e.company_id
+group by c.name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
